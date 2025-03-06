e1236c0a127314fd72e1733db537d815c4477583
albertoojeda-creater <alberto.ojeda.ao2001@gmail.com>
2025-03-06 09:03:14 -0600

    Modificación en sign y login, con verificación e implementación de images

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -63,111 +63,140 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
+    // Tamaño de la pantalla
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Iniciar sesión')),
       body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12.0),
-          ),
-          elevation: 10.0,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  const Center(
-                    child: Text(
-                      'Iniciar sesión',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 30.0),
-
-                  // Campo Email
-                  TextFormField(
-                    controller: _emailController,
-                    decoration: const InputDecoration(
-                      labelText: 'Correo electrónico',
-                      border: OutlineInputBorder(),
-                      contentPadding: EdgeInsets.symmetric(
-                          vertical: 15.0, horizontal: 10.0),
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor ingrese su correo electrónico';
-                      }
-                      if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                        return 'Por favor ingrese un correo válido';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-
-                  // Campo Contraseña
-                  TextFormField(
-                    controller: _passwordController,
-                    decoration: const InputDecoration(
-                      labelText: 'Contraseña',
-                      border: OutlineInputBorder(),
-                      contentPadding: EdgeInsets.symmetric(
-                          vertical: 15.0, horizontal: 10.0),
-                    ),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor ingrese su contraseña';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 30.0),
-
-                  // Botón de Login
-                  Center(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        minimumSize:
-                            const Size(double.infinity, 50), // Ancho completo
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                      onPressed: _iniciarSesion,
-                      child: const Text(
-                        'Iniciar sesión',
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20.0),
-
-                  // Texto adicional para redirigir a registro
-                  Center(
-                    child: TextButton(
-                      onPressed: () {
-                        // Redirigir a la pantalla de registro
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Registro()),
-                        );
-                      },
-                      child: const Text('¿No tienes cuenta? Regístrate'),
-                    ),
-                  ),
-                ],
+        child: Column(
+          children: [
+            // Imagen más grande y centrada
+            Center(
+              child: Image.asset(
+                'assets/images/logotipo.png', // Asegúrate de que esta ruta sea correcta
+                height: screenHeight *
+                    0.45, // Hacemos la imagen más grande, ocupando el 40% de la altura
               ),
             ),
-          ),
+            // Caja de formulario con bordes redondeados
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                elevation: 6.0, // Elevación más suave
+                child: Padding(
+                  padding: const EdgeInsets.all(
+                      16.0), // Padding más pequeño para el card
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const Center(
+                          child: Text(
+                            'Iniciar sesión',
+                            style: TextStyle(
+                              fontSize:
+                                  16, // Tamaño de la fuente ajustado para que no sea tan grande
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 14.0),
+
+                        // Campo Email
+                        TextFormField(
+                          controller: _emailController,
+                          decoration: const InputDecoration(
+                            labelText: 'Correo electrónico',
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 12.0, horizontal: 10.0),
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor ingrese su correo electrónico';
+                            }
+                            if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                                .hasMatch(value)) {
+                              return 'Por favor ingrese un correo válido';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 14.0),
+
+                        // Campo Contraseña
+                        TextFormField(
+                          controller: _passwordController,
+                          decoration: const InputDecoration(
+                            labelText: 'Contraseña',
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 12.0, horizontal: 10.0),
+                          ),
+                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor ingrese su contraseña';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 14.0),
+
+                        // Botón de Login
+                        Center(
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(
+                                screenWidth *
+                                    0.65, // Ajustamos el tamaño del botón al 85% del ancho de la pantalla
+                                50,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            onPressed: _iniciarSesion,
+                            child: const Text(
+                              'Iniciar sesión',
+                              style: TextStyle(
+                                  fontSize: 14.0), // Reducir tamaño del texto
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 14.0),
+
+                        // Texto adicional para redirigir a registro
+                        Center(
+                          child: TextButton(
+                            onPressed: () {
+                              // Redirigir a la pantalla de registro
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Registro()),
+                              );
+                            },
+                            child: const Text(
+                              '¿No tienes cuenta? Regístrate',
+                              style: TextStyle(
+                                  fontSize: 12.0), // Reducir tamaño del texto
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/pages/sign_page.dart
+++ lib/pages/sign_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:caribe/model/user_class.dart'; // Asegúrate de importar el modelo User
 import 'package:caribe/pages/login_page.dart'; // Importamos la pantalla de Login
+import 'package:caribe/pages/home_page.dart'; // Importamos la pantalla de Home
+import 'package:caribe/pages/home_art_page.dart'; // Importamos la pantalla de Home para artesanos
 
 class Registro extends StatefulWidget {
   const Registro({super.key});
@@ -32,153 +34,190 @@ class _RegistroState extends State<Registro> {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Registro exitoso')),
       );
+
+      // Redirigir dependiendo de si es artesano o usuario
+      if (_esArtesano) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const ArtesanoHome()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const Home()),
+        );
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    // Tamaño de la pantalla
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Registro de Usuario')),
       body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12.0),
-          ),
-          elevation: 10.0,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  const Center(
-                    child: Text(
-                      'Crea una cuenta',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 30.0),
-
-                  // Campo Nombre
-                  TextFormField(
-                    controller: _nombreController,
-                    decoration: const InputDecoration(
-                      labelText: 'Nombre completo',
-                      border: OutlineInputBorder(),
-                      contentPadding: EdgeInsets.symmetric(
-                          vertical: 15.0, horizontal: 10.0),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor ingrese su nombre';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-
-                  // Campo Email
-                  TextFormField(
-                    controller: _emailController,
-                    decoration: const InputDecoration(
-                      labelText: 'Correo electrónico',
-                      border: OutlineInputBorder(),
-                      contentPadding: EdgeInsets.symmetric(
-                          vertical: 15.0, horizontal: 10.0),
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor ingrese su correo electrónico';
-                      }
-                      if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                        return 'Por favor ingrese un correo válido';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-
-                  // Campo Contraseña
-                  TextFormField(
-                    controller: _passwordController,
-                    decoration: const InputDecoration(
-                      labelText: 'Contraseña',
-                      border: OutlineInputBorder(),
-                      contentPadding: EdgeInsets.symmetric(
-                          vertical: 15.0, horizontal: 10.0),
-                    ),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor ingrese su contraseña';
-                      }
-                      if (value.length < 6) {
-                        return 'La contraseña debe tener al menos 6 caracteres';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-
-                  // Checkbox para determinar si es artesano
-                  Row(
-                    children: [
-                      Checkbox(
-                        value: _esArtesano,
-                        onChanged: (value) {
-                          setState(() {
-                            _esArtesano = value!;
-                          });
-                        },
-                      ),
-                      const Text('¿Eres artesano?'),
-                    ],
-                  ),
-                  const SizedBox(height: 30.0),
-
-                  // Botón de Registro
-                  Center(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        minimumSize:
-                            const Size(double.infinity, 50), // Ancho completo
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                      onPressed: _registrarUsuario,
-                      child: const Text(
-                        'Registrar',
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20.0),
-
-                  // Texto adicional, como link para login
-                  Center(
-                    child: TextButton(
-                      onPressed: () {
-                        // Redirigir a la pantalla de login
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Login()),
-                        );
-                      },
-                      child: const Text('¿Ya tienes cuenta? Iniciar sesión'),
-                    ),
-                  ),
-                ],
+        child: Column(
+          children: [
+            // Imagen más grande y centrada
+            Center(
+              child: Image.asset(
+                'assets/images/logotipo.png', // Asegúrate de que esta ruta sea correcta
+                height: screenHeight *
+                    0.45, // Hacemos la imagen más grande, ocupando el 45% de la altura
               ),
             ),
-          ),
+            // Caja de formulario con bordes redondeados
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                elevation: 6.0, // Elevación más suave
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const Center(
+                          child: Text(
+                            'Crea una cuenta',
+                            style: TextStyle(
+                              fontSize: 16, // Tamaño de la fuente ajustado
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 14.0),
+
+                        // Campo Nombre
+                        TextFormField(
+                          controller: _nombreController,
+                          decoration: const InputDecoration(
+                            labelText: 'Nombre completo',
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 12.0, horizontal: 10.0),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor ingrese su nombre';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 14.0),
+
+                        // Campo Email
+                        TextFormField(
+                          controller: _emailController,
+                          decoration: const InputDecoration(
+                            labelText: 'Correo electrónico',
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 12.0, horizontal: 10.0),
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor ingrese su correo electrónico';
+                            }
+                            if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                                .hasMatch(value)) {
+                              return 'Por favor ingrese un correo válido';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 14.0),
+
+                        // Campo Contraseña
+                        TextFormField(
+                          controller: _passwordController,
+                          decoration: const InputDecoration(
+                            labelText: 'Contraseña',
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 12.0, horizontal: 10.0),
+                          ),
+                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor ingrese su contraseña';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 14.0),
+
+                        // Checkbox para determinar si es artesano
+                        Row(
+                          children: [
+                            Checkbox(
+                              value: _esArtesano,
+                              onChanged: (value) {
+                                setState(() {
+                                  _esArtesano = value!;
+                                });
+                              },
+                            ),
+                            const Text('¿Eres artesano?'),
+                          ],
+                        ),
+                        const SizedBox(height: 14.0),
+
+                        // Botón de Registro
+                        Center(
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(
+                                screenWidth *
+                                    0.65, // Ajustamos el tamaño del botón al 65% del ancho de la pantalla
+                                50,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            onPressed: _registrarUsuario,
+                            child: const Text(
+                              'Registrar',
+                              style: TextStyle(
+                                  fontSize: 14.0), // Reducir tamaño del texto
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 14.0),
+
+                        // Texto adicional para redirigir a login
+                        Center(
+                          child: TextButton(
+                            onPressed: () {
+                              // Redirigir a la pantalla de login
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Login()),
+                              );
+                            },
+                            child: const Text(
+                              '¿Ya tienes cuenta? Iniciar sesión',
+                              style: TextStyle(
+                                  fontSize: 12.0), // Reducir tamaño del texto
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
